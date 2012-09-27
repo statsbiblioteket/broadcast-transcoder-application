e39d008affda9b92129d6784fc4278f67c38d9cd
Colin Rosenthal <colin@wigner.(none)>
2012-09-27 09:00:10 +0200

    Added property names

--- src/main/java/dk/statsbiblioteket/broadcasttranscoder/cli/OptionsParser.java
+++ src/main/java/dk/statsbiblioteket/broadcasttranscoder/cli/OptionsParser.java
@@ -9,6 +9,12 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import static dk.statsbiblioteket.broadcasttranscoder.cli.PropertyNames.*;
 
 /**
  *
@@ -16,9 +22,7 @@ import org.apache.commons.cli.PosixParser;
 public class OptionsParser {
 
     protected static final Option PID_OPTION = new Option("programpid", true, "The DOMS pid of the program to be transcoded");
-    protected static final Option DOMS_ENDPOINT_OPTION = new Option("domsendpoint", true, "The service location for the DOMS endpoint");
-    protected static final Option VIDEO_BITRATE_OPTION = new Option("videobitrate", true, "The required video bitrate (kbps)");
-
+    protected static final Option CONFIG_FILE_OPTION = new Option("configfile", true, "The config file");
     protected static Options options;
 
     private Context context;
@@ -27,8 +31,7 @@ public class OptionsParser {
         context = new Context();
         options = new Options();
         options.addOption(PID_OPTION);
-        options.addOption(DOMS_ENDPOINT_OPTION);
-        options.addOption(VIDEO_BITRATE_OPTION);
+        options.addOption(CONFIG_FILE_OPTION);
     }
 
 
@@ -42,19 +45,45 @@ public class OptionsParser {
             throw new OptionParseException(e.getMessage(), e);
         }
         parseProgramPid(cmd);
-        parseDOMSEndpoint(cmd);
-        parseVideoBitrate(cmd);
+        parseConfigFileOption(cmd);
+        try {
+            readProperties(context);
+        } catch (IOException e) {
+            throw new OptionParseException("Error reading properties.", e);
+        }
         return context;
     }
 
-    protected void parseDOMSEndpoint(CommandLine cmd) throws OptionParseException {
-        String domsEndpoint = cmd.getOptionValue(DOMS_ENDPOINT_OPTION.getOpt());
-        if (domsEndpoint == null) {
-            parseError(DOMS_ENDPOINT_OPTION.toString());
-            throw new OptionParseException(DOMS_ENDPOINT_OPTION.toString());
-        } else {
-            context.setDomsEndpoint(domsEndpoint);
+    protected void readProperties(Context context) throws IOException, OptionParseException {
+        Properties props = new Properties();
+        props.load(new FileInputStream(context.getConfigFile()));
+        context.setVideoBitrate(readIntegerProperty(VIDEO_BITRATE, props));
+    }
+
+    protected int readIntegerProperty(String propName, Properties props) throws OptionParseException {
+        String prop = props.getProperty(propName);
+        try {
+            Integer result = Integer.parseInt(prop);
+            return result;
+        } catch (NumberFormatException e) {
+            throw new OptionParseException("Cannot parse " + prop + " as an integer.");
         }
+
+    }
+
+
+
+    protected void parseConfigFileOption(CommandLine cmd) throws OptionParseException {
+        String configFileString = cmd.getOptionValue(CONFIG_FILE_OPTION.getOpt());
+        if (configFileString == null) {
+            parseError(CONFIG_FILE_OPTION.toString());
+            throw new OptionParseException(CONFIG_FILE_OPTION.toString());
+        }
+        File configFile = new File(configFileString);
+        if (!configFile.exists() || configFile.isDirectory()) {
+            throw new OptionParseException(configFile.getAbsolutePath() + " is not a file.");
+        }
+        context.setConfigFile(configFile);
     }
 
     protected void parseProgramPid(CommandLine cmd) throws OptionParseException {
@@ -66,14 +95,6 @@ public class OptionsParser {
             context.setProgrampid(programPid);
         }
     }
-
-    protected void parseVideoBitrate(CommandLine cmd) {
-        String videoBitrate = cmd.getOptionValue(VIDEO_BITRATE_OPTION.getOpt());
-        if (videoBitrate != null) {
-            context.setVideoBitrate(Integer.parseInt(videoBitrate));
-        }
-    }
-
 
     protected void printUsage() {
         final HelpFormatter usageFormatter = new HelpFormatter();
