fd0f0c61a4ad1d62ad8cc430a474cd552880192b
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-11-21 16:47:15 +0100

    Restructured a bit about option parsing

---
+++ broadcast-transcoder/src/main/java/dk/statsbiblioteket/broadcasttranscoder/cli/AbstractOptionsParser.java
@@ -0,0 +1,49 @@
+package dk.statsbiblioteket.broadcasttranscoder.cli;
+
+import java.io.File;
+import java.util.Properties;
+
+public  abstract class AbstractOptionsParser {
+    public AbstractOptionsParser() {
+    }
+
+    protected File readExistingDirectoryProperty(String propName, Properties props) throws OptionParseException {
+        String prop = props.getProperty(propName);
+        if (prop == null || "".equals(prop)) {
+            throw new OptionParseException("Property " + propName + " not set.");
+        }
+        File dir = new File(prop);
+        if (!dir.exists() || !dir.isDirectory()) {
+            throw new OptionParseException(dir.getAbsolutePath() + " must be a pre-existing directory");
+        }
+        return dir;
+    }
+
+    protected String readStringProperty(String propName, Properties props) throws OptionParseException {
+        String prop = props.getProperty(propName);
+        if (prop == null || "".equals(prop)) {
+            throw new OptionParseException("Property " + propName + " not set.");
+        }
+        return prop;
+    }
+
+    protected int readIntegerProperty(String propName, Properties props) throws OptionParseException {
+        String prop = props.getProperty(propName);
+        try {
+            Integer result = Integer.parseInt(prop);
+            return result;
+        } catch (NumberFormatException e) {
+            throw new OptionParseException("Cannot parse " + prop + " as an integer.");
+        }
+    }
+
+    protected long readLongProperty(String propName, Properties props) throws OptionParseException {
+        String prop = props.getProperty(propName);
+        try {
+            Long result = Long.parseLong(prop);
+            return result;
+        } catch (NumberFormatException e) {
+            throw new OptionParseException("Cannot parse " + prop + " as a long.");
+        }
+    }
+}
