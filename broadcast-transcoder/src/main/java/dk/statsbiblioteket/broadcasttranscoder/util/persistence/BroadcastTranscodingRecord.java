8bf3d80407d49c8152857cd88e73ac6da009dfd7
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-12-17 13:36:28 +0100

    Added equals and toString methods to the TranscodingRecord

--- broadcast-transcoder/src/main/java/dk/statsbiblioteket/broadcasttranscoder/util/persistence/BroadcastTranscodingRecord.java
+++ broadcast-transcoder/src/main/java/dk/statsbiblioteket/broadcasttranscoder/util/persistence/BroadcastTranscodingRecord.java
@@ -107,4 +107,62 @@ public class BroadcastTranscodingRecord {
        private String title;
        private String channel;
 
+
+    @Override
+    public String toString() {
+        return "BroadcastTranscodingRecord{" +
+                "domsProgramPid='" + domsProgramPid + '\'' +
+                ", lastTranscodedTimestamp=" + lastTranscodedTimestamp +
+                ", transcodingCommand='" + transcodingCommand + '\'' +
+                ", isTvmeter=" + isTvmeter +
+                ", startOffset=" + startOffset +
+                ", endOffset=" + endOffset +
+                ", broadtcastStartTime=" + broadtcastStartTime +
+                ", broadcastEndTime=" + broadcastEndTime +
+                ", title='" + title + '\'' +
+                ", channel='" + channel + '\'' +
+                '}';
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BroadcastTranscodingRecord)) return false;
+
+        BroadcastTranscodingRecord that = (BroadcastTranscodingRecord) o;
+
+        if (endOffset != that.endOffset) return false;
+        if (isTvmeter != that.isTvmeter) return false;
+        if (startOffset != that.startOffset) return false;
+        if (broadcastEndTime != null ? !broadcastEndTime.equals(that.broadcastEndTime) : that.broadcastEndTime != null)
+            return false;
+        if (broadtcastStartTime != null ? !broadtcastStartTime.equals(that.broadtcastStartTime) : that.broadtcastStartTime != null)
+            return false;
+        if (channel != null ? !channel.equals(that.channel) : that.channel != null) return false;
+        if (domsProgramPid != null ? !domsProgramPid.equals(that.domsProgramPid) : that.domsProgramPid != null)
+            return false;
+        if (lastTranscodedTimestamp != null ? !lastTranscodedTimestamp.equals(that.lastTranscodedTimestamp) : that.lastTranscodedTimestamp != null)
+            return false;
+        if (title != null ? !title.equals(that.title) : that.title != null) return false;
+        if (transcodingCommand != null ? !transcodingCommand.equals(that.transcodingCommand) : that.transcodingCommand != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = domsProgramPid != null ? domsProgramPid.hashCode() : 0;
+        result = 31 * result + (lastTranscodedTimestamp != null ? lastTranscodedTimestamp.hashCode() : 0);
+        result = 31 * result + (transcodingCommand != null ? transcodingCommand.hashCode() : 0);
+        result = 31 * result + (isTvmeter ? 1 : 0);
+        result = 31 * result + startOffset;
+        result = 31 * result + endOffset;
+        result = 31 * result + (broadtcastStartTime != null ? broadtcastStartTime.hashCode() : 0);
+        result = 31 * result + (broadcastEndTime != null ? broadcastEndTime.hashCode() : 0);
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (channel != null ? channel.hashCode() : 0);
+        return result;
+    }
 }
