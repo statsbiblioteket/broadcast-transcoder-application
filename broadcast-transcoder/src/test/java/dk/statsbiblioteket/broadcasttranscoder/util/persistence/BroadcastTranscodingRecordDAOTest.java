719fe9fbdb82bd09471bbe13444afeaf274fae81
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-12-17 13:37:28 +0100

    Test more of the functionality of the transcode request persister

--- broadcast-transcoder/src/test/java/dk/statsbiblioteket/broadcasttranscoder/util/persistence/BroadcastTranscodingRecordDAOTest.java
+++ broadcast-transcoder/src/test/java/dk/statsbiblioteket/broadcasttranscoder/util/persistence/BroadcastTranscodingRecordDAOTest.java
@@ -1,8 +1,13 @@
 package dk.statsbiblioteket.broadcasttranscoder.util.persistence;
 
 import dk.statsbiblioteket.broadcasttranscoder.cli.Context;
+import dk.statsbiblioteket.broadcasttranscoder.domscontent.ProgramBroadcast;
+import dk.statsbiblioteket.broadcasttranscoder.processors.TranscodeRequest;
+import dk.statsbiblioteket.broadcasttranscoder.util.CalendarUtils;
+import dk.statsbiblioteket.broadcasttranscoder.util.MetadataUtils;
 import junit.framework.TestCase;
 
+import javax.xml.datatype.XMLGregorianCalendar;
 import java.io.File;
 import java.util.Date;
 
@@ -15,18 +20,78 @@ import java.util.Date;
  */
 public class BroadcastTranscodingRecordDAOTest extends TestCase {
 
-    public void testSetTimestamp() throws Exception {
-        Context context = new Context();
+    Context context = new Context();
+    TranscodeRequest request = new TranscodeRequest();
 
+
+    public void setUp() throws Exception {
+        super.setUp();    //To change body of overridden methods use File | Settings | File Templates.
         context.setHibernateConfigFile(new File(Thread.currentThread().getContextClassLoader().getResource("hibernate-derby.xml").toURI()));
+        context.setProgrampid("uuid:test1");
+        request.setTvmeter(false);
+        request.setEndOffsetUsed(0);
+        request.setStartOffsetUsed(10);
+        request.setTitle("test transcoding");
+        ProgramBroadcast programBroadcast = new ProgramBroadcast();
+        programBroadcast.setChannelId("dr1");
+        XMLGregorianCalendar startCal = CalendarUtils.getCalendar();
+        XMLGregorianCalendar endCal = CalendarUtils.getCalendar();
+
+
+        programBroadcast.setTimeStart(startCal);
+        programBroadcast.setTimeStop(endCal);
+        startCal.setHour(13);
+        endCal.setHour(14);
+        request.setProgramBroadcast(programBroadcast);
+
+    }
+
+    public void testSetTimestamp() throws Exception {
         HibernateUtil util = HibernateUtil.getInstance(context.getHibernateConfigFile().getAbsolutePath());
         context.setTimestampPersister(new BroadcastTranscodingRecordDAO(util));
         TimestampPersister persister = context.getTimestampPersister();
         long timestamp = new Date().getTime();
-        persister.setTimestamp("uuid:testid1",timestamp);
 
-        Long retrieved = persister.getTimestamp("uuid:testid1");
+        Long temp = persister.getTimestamp(context.getProgrampid());
+        assertNull("Database is not empty",temp);
+
+        persister.setTimestamp(context.getProgrampid(),timestamp);
+
+        Long retrieved = persister.getTimestamp(context.getProgrampid());
         assertTrue("timestamps not equal after save",timestamp ==retrieved );
+
+    }
+
+    public void testSomthing(){
+
+
+        HibernateUtil util = HibernateUtil.getInstance(context.getHibernateConfigFile().getAbsolutePath());
+        BroadcastTranscodingRecordDAO dao = new BroadcastTranscodingRecordDAO(util);
+        Long temp = dao.getTimestamp(context.getProgrampid());
+        assertNull("Database is not empty",temp);
+
+
+
+        dao.setTimestamp(context.getProgrampid(),new Date().getTime());
+        BroadcastTranscodingRecord record = dao.read(context.getProgrampid());
+
+
+
+        record.setTvmeter(request.isTvmeter());
+        record.setBroadtcastStartTime(MetadataUtils.getProgramStart(request));
+        record.setBroadcastEndTime(MetadataUtils.getProgramEnd(request));
+        record.setChannel(request.getProgramBroadcast().getChannelId());
+        record.setEndOffset(request.getEndOffsetUsed());
+        record.setStartOffset(request.getStartOffsetUsed());
+        record.setTitle(request.getTitle());
+        dao.update(record);
+
+        BroadcastTranscodingRecord record2 = dao.read(context.getProgrampid());
+
+        System.out.println(record);
+        System.out.println(record2);
+        assertEquals("object changed by persistence",record,record2);
+
 
     }
 }
