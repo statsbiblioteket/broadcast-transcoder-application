3a02315b33938b42379178c1410dc925a5a6b011
Colin Rosenthal <csr@statsbiblioteket.dk>
2013-01-22 15:12:05 +0100

    Minor scripting fixes

--- broadcast-transcoder-commandline/src/main/scripts/transcodeChanges.sh
+++ broadcast-transcoder-commandline/src/main/scripts/transcodeChanges.sh
@@ -20,48 +20,62 @@ fi
 # Cleanup from previous run
 
 function cleanup {
-[ $debug = 1 ] && echo Cleaning Up
+[ $debug = 1 ] && echo "Cleaning Up"
 for ((i=0;i<$WORKERS;i++)); do
     workerfile="$workDir/$i$collection.workerFile"
     if [ -e  "$workerfile" ]; then
-    [ $debug = 1 ] && echo Cleaning from $workerfile
-    pid=$(cat $workerfile|cut -d' ' -f1)
-	uuid=$(cat $workerfile|cut -d ' ' -f2)
-	timestamp=$(cat $workerfile|cut -d ' ' -f3)
-	machine=$(cat $workerfile|cut -d ' ' -f4)
-	[ $debug = 1 ] && echo Cleaning up after $uuid
-    kill $pid
-	lockfile "$failureFile.lock"
-	echo "$collection $uuid $timestamp" >> $failureFile
-	rm -f "$failureFile.lock"
-	rm $workerfile
-	$SCRIPT_PATH/cleanupUnfinished.sh $uuid $timestamp $machine
-	lockfile "$progressFile.lock"
-    current_progress_timestamp=$(cat $progressFile)
-    #
-    # Check whether the process to be cleaned up is from an earlier timestamp and,
-    # if necessary, set the clock of progress back.
-    #
-    if [ $timestamp -lt $current_progress_timestamp ]; then
-       echo $(( $timestamp - 1)) > $progressFile
-    fi
-    echo  "$collection" "$uuid" "$timestamp"  >> $stateDir/$collection.successes
-    rm -f "$progressFile.lock"
+        [ $debug = 1 ] && echo Cleaning from $workerfile
+        pid=$(cat $workerfile|cut -d' ' -f1)
+        uuid=$(cat $workerfile|cut -d ' ' -f2)
+        timestamp=$(cat $workerfile|cut -d ' ' -f3)
+        machine=$(cat $workerfile|cut -d ' ' -f4)
+        [ $debug = 1 ] && echo Cleaning up after $uuid
+        kill $pid
+        rm $workerfile
+        $SCRIPT_PATH/cleanupUnfinished.sh $uuid $timestamp $machine
     fi
 done
 }
 
 cleanup;
-trap 'cleanup; rmdir $globalLock; rm -r $changes; exit 1' INT TERM
+trap 'cleanup; rmdir $globalLock; rm -rf $changes; exit 1' INT TERM
 
 ##
 # This is probably not necessary, unless we have an unusual race condition that leaves lockfiles behind from a
 # previous run.
 rm -f $workDir/*${collection}*.lock
 
+
+# get lowest timestamp from progressfiles
+timestamp=$(date +%s)000  #Everything should be less than this
+seedFile=$stateDir/$collection.seed
+for ((i=0;i<$WORKERS;i++)); do
+    progressFile=$stateDir/$i.$collection.progress
+    if [ -e $progressFile ]; then
+        progress_timestamp=$(cat $progressFile)
+    else
+        if [ -e $seedFile ]; then
+            progress_timestamp=$(cat $seedFile)
+        else
+          echo "No seedfile $seedFile found. Exiting."
+         exit 1;
+        fi
+    fi
+    if [ $progress_timestamp -lt $timestamp ]; then
+        timestamp=$progress_timestamp
+    fi
+done
+for ((i=0;i<$WORKERS;i++)); do
+    progressFile=$stateDir/$i.$collection.progress
+    echo $timestamp > $progressFile
+done
+if [ -e $seedFile ]; then
+   rm ${seedFile}
+fi
+
 # Get list of changes from queryChanges with progress timestamp as input
-timestamp=$(cat $progressFile)
 changes=$( mktemp -p $workDir )
+[ $debug = 1 ] && echo Querying changes since $timestamp
 $SCRIPT_PATH/queryChanges.sh $collection $timestamp | grep "^uuid" > $changes
 
 # Cut list into pid/timestamp sets
@@ -80,67 +94,68 @@ while read uuid time; do
     started=0
     # Iterate over workers until one picks up the current uuid
     while [ $started -eq 0 ]; do
-	[ $debug = 1 ] && echo "$uuid: Starting allocation to worker for uuid $uuid"
-	for ((i=0;i<$WORKERS;i++)); do
-	    workerfile="$workDir/$i$collection.workerFile"
-	    [ $debug = 1 ] && echo "$uuid: Attempting to get lock on $workerfile"
-	    lockfile "$workerfile.lock"
-	    if [ ! -e $workerfile ]; then
-		[ $debug = 1 ] && echo "$uuid: $workerfile does not exist, creating"
-		touch $workerfile
-	    fi
+        [ $debug = 1 ] && echo "$uuid: Starting allocation to worker for uuid $uuid"
+        for ((i=0;i<$WORKERS;i++)); do
+            workerfile="$workDir/$i$collection.workerFile"
+            [ $debug = 1 ] && echo "$uuid: Attempting to get lock on $workerfile"
+            lockfile "$workerfile.lock"
+            if [ ! -e $workerfile ]; then
+                [ $debug = 1 ] && echo "$uuid: $workerfile does not exist, creating"
+                touch $workerfile
+                found=1
+            else
+                pid=$(cat $workerfile|cut -d' ' -f1)
+                workeruuid=$(cat $workerfile|cut -d' ' -f2)
+                workertime=$(cat $workerfile|cut -d' ' -f3)
+                [ $debug = 1 ] && echo "$uuid: Got pid '$pid' from $workerfile"
 
-	    pid=$(cat $workerfile|cut -d' ' -f1)
-	    [ $debug = 1 ] && echo "$uuid: Got pid '$pid' from $workerfile"
-
-	    kill -0 $pid &> /dev/null
-	    found=$?
-
-	    if [ $found != 0 ]; then # pid is no longer running or does not belong to us
-		machine=${machines[$machineIndex]}
-		[ $debug = 1 ] && echo "$uuid: Did not find found '$pid' among the running processes"
-		[ $debug = 1 ] && echo "$uuid: Starting transcoding for $uuid and $time on $machine"
-		$SCRIPT_PATH/transcoderWorker.sh $collection $uuid $time $machine &
-		echo "$! $uuid $time $machine" > $workerfile
-		# Increment the machine index
-		((machineIndex++))
-		max=${#machines[*]}
-		[ $machineIndex -ge $max ] && machineIndex=0
-		started=1
-	    fi
-	    [ $debug = 1 ] && echo "$uuid: releasing lock on $workerfile"
-	    rm -f "$workerfile.lock"
-	    if [ $started -gt 0 ]; then
-		[ $debug = 1 ] && echo "$uuid: Allocated to worker, moving to next entity"
-		break 1
-	    fi
-	done
-	sleep 2
-	echo
+                kill -0 $pid &> /dev/null
+                found=$?
+                if [ $found != 0 ]; then #workerfile found, but proces stopped
+                    #the content of workerfile never completed. Add it to the incomplete list
+                    echo $workeruuid $workertime >> $stateDir/$collection.incompletes
+                    # TODO log this
+                fi
+            fi
+            if [ $found != 0 ]; then # pid is no longer running or does not belong to us
+                machine=${machines[$machineIndex]}
+                [ $debug = 1 ] && echo "$uuid: Did not find found '$pid' among the running processes"
+                [ $debug = 1 ] && echo "$uuid: Starting transcoding for $uuid and $time on $machine"
+                $SCRIPT_PATH/transcoderWorker.sh $collection $uuid $time $machine $i &
+                echo "$! $uuid $time $machine" > $workerfile
+                # Increment the machine index
+                ((machineIndex++))
+                max=${#machines[*]}
+                [ $machineIndex -ge $max ] && machineIndex=0
+                started=1
+            fi
+            [ $debug = 1 ] && echo "$uuid: releasing lock on $workerfile"
+            rm -f "$workerfile.lock"
+            if [ $started -gt 0 ]; then
+                [ $debug = 1 ] && echo "$uuid: Allocated to worker, moving to next entity"
+                break 1
+            fi
+        done
+        sleep 2
+        echo
     done
     echo
 done < $changes
 
-all_finished=$(false)
-while ( !all_finished ); do
+while ( true ); do
    sleep 2
-   workerFiles="$workDir/*$collection.workerFile"
-   set $workerFiles
-   if [ $# -eq 0 ]; then
-       all_finished=$(true)
+   workerFiles=$(ls -1 $workDir/*$collection.workerFile 2>/dev/null | wc -l)
+   if [  $workerFiles -eq 0 ]; then
+       break 1
    else
-      for workerfile in $workerFiles; do
-         pid=$(cat $workerfile|cut -d' ' -f1)
-         kill -0 $pid &> /dev/null
-	     found=$?
-	     if [ $found != 0 ]; then
-	         [ $debug = 1 ] && echo "$uuid: Did not find '$pid' among the running processes"
-             [ $debug = 1 ] && echo "Deleting worker file $workerfile"
-             rm $workerfile
-	     fi
-      done
+     [ $debug = 1 ] && echo "Waiting for $workerFiles"
    fi
 done
+
+# TODO find incompletes in successes and remove them from incompletes
+
+# Merge state files here
+
 echo "All transcoding finished. Exiting normally."
 rm $changes
 rmdir $globalLock
