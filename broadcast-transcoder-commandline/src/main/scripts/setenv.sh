f43771140aedbc50bf3f741db679aaa2949a87db
Tom G. Christensen <tgc@statsbiblioteket.dk>
2013-02-26 14:31:38 +0100

    Trim setenv.sh to match current users

--- broadcast-transcoder-commandline/src/main/scripts/setenv.sh
+++ broadcast-transcoder-commandline/src/main/scripts/setenv.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-# The number of worker processes. If this is changed from a previous run then the
-# cleanup loop needs to be executed by hand.
-WORKERS=2
-
-# These are external machines on which to run the transcoding process by ssh eg user1@encoder1 ....
-# The special value "local" means "do the transcoding locally as the current user without ssh"
-#
-# It is a requirement that the installation directory for bta on the remote machine has the same absolute
-# path as the local installation where the scripts run.
-# machines=( "bta@iapetus" )
-machines=( "local" )
-
-#
-# Set debug=1 for debugging output from scripts, debug=0 for minimal output.
-#
-debug=0
+# Settings file for the java programs included in BTA
 
 # This script expects SCRIPT_PATH to be defined, if it is not then notify
 # and error out
@@ -26,26 +11,15 @@ fi
 
 # Set CLASSPATH for the JVM
 CLASSPATH="$SCRIPT_PATH/../lib/*"
-# Logs go here
-logDir="$HOME/logs"
 # Config is here
 confDir="$SCRIPT_PATH/../config"
-# work-in-progress logs and other temporary files like .lock files go here
-workDir=$HOME/var/work
-# stateDir, this holds permanent state, such as the various progress files
-stateDir=$HOME/var/state
 
 ###############################################################################
 # Nothing on or below this line should need to be changed for configuration purposes.
 
-# Make sure directories exist
-for setenv_mkdir in $logDir $workDir $stateDir; do
-    [ ! -d "$setenv_mkdir" ] && mkdir -p "$setenv_mkdir"
-done
-
 # collection can have values "Broadcast" or "Reklamefilm"
 # if it is unset or empty then we cannot proceed
-collection=${collection:-undefined}
+collection=${collection:-<undefined>}
 case $collection in
     Broadcast|Reklamefilm)
 	# A valid collection given
