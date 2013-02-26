6cd46c003f781ccf10c11514af93be48d963456a
Colin Rosenthal <csr@statsbiblioteket.dk>
2013-02-26 11:51:22 +0100

    Skeleton for REST webservice.

---
+++ broadcast-transcoder-webservice/src/main/java/dk/statsbiblioteket/broadcasttranscoder/ws/BroadcastTranscoderService.java
@@ -0,0 +1,21 @@
+package dk.statsbiblioteket.broadcasttranscoder.ws;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/** Example resource class hosted at the URI path "/myresource"
+ */
+@Path("/myresource")
+public class BroadcastTranscoderService {
+    
+    /** Method processing HTTP GET requests, producing "text/plain" MIME media
+     * type.
+     * @return String that will be send back as a response of type "text/plain".
+     */
+    @GET 
+    @Produces("text/plain")
+    public String getIt() {
+        return "Hi there!";
+    }
+}
