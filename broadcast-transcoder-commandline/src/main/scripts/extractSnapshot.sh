282b9224c45a86161bbe31f7fb4cb97360b36c57
Colin Rosenthal <csr@statsbiblioteket.dk>
2014-03-13 11:46:34 +0100

    Refactored thumbnailer into main project.

--- broadcast-transcoder-commandline/src/main/scripts/extractSnapshot.sh
+++ broadcast-transcoder-commandline/src/main/scripts/extractSnapshot.sh
@@ -10,7 +10,7 @@ source $SCRIPT_PATH/setenv.sh
 
 java -Dlogback.configurationFile=$confDir/logback-extractSnapshot.xml $hibernate_log_config \
  -cp "$CLASSPATH" \
-  dk.statsbiblioteket.broadcasttranscoder.thumbnailer.BroadcastThumbnailApplication \
+  dk.statsbiblioteket.broadcasttranscoder.BroadcastThumbnailApplication \
  --hibernate_configfile=$confDir/hibernate.cfg.xml\
  --infrastructure_configfile=$confDir/bta.infrastructure.properties \
  --behavioural_configfile=$confDir/bta.behaviour.properties \
