2622871f3be93e7c64164cb8fc87d73dec1e964d
Asger Askov Blekinge <abr@kb.dk>
2018-05-08 17:25:15 +0200

    Script to deploy system on devel and run test

--- deploy_on_devel.sh
+++ deploy_on_devel.sh
@@ -17,10 +17,20 @@ scp broadcast-transcoder-commandline/target/$archive $develHost:.
 #TODO perhaps delete existing install first?
 ssh $develHost "tar -xvzf $archive --directory /home/bta/bta --strip 1"
 
+ssh $develHost 'rm -f streamingContent/*.mp3'
+ssh $develHost 'rm -f streamingContent/*.flv'
+ssh $develHost 'rm -f imageDirectory/*.png'
+
+ssh $develHost 'rm -f logs/* || true'
+
 #Initialisation
-ssh $develHost "psql -d bta-devel -c 'delete from broadcasttranscodingrecord;'"
+#ssh $develHost "psql -d bta-devel -c 'delete from broadcasttranscodingrecord;'"
+#ssh $develHost 'bta/bin/enqueueJobs.sh Broadcast 0'
+#ssh $develHost 'bta/bin/queryChangesDoms.sh Broadcast > ~/queue$(date +"%y%m%d").txt'
+ssh $develHost 'head -n4 ~/queue$(date +"%y%m%d").txt > ~/queue.txt'
+ssh $develHost 'bta/bin/transcode-master.sh start -h localhost -n 1 -j ~/queue.txt -v'
 
-ssh $develHost 'rm streamingContent/*.mp3'
-ssh $develHost 'rm streamingContent/*.flv'
 
-ssh $develHost 'rm logs/*'
+# Stop trancodering:
+# bta/bin/transcode-master.sh stop
+# ps -ef | grep transcoder | grep -v grep| sed 's/ \+/ /g' | cut -d' ' -f2 | xargs -r kill
