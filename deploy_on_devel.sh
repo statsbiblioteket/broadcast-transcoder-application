cdf701295f3fde8733f3b3ad521332d1496d8310
Asger Askov Blekinge <abr@kb.dk>
2018-07-12 18:25:25 +0200

    Deploy on devel tests teracom transcode

--- deploy_on_devel.sh
+++ deploy_on_devel.sh
@@ -24,13 +24,19 @@ ssh $develHost 'rm -f imageDirectory/*.png'
 ssh $develHost 'rm -f logs/* || true'
 
 #Initialisation
-#ssh $develHost "psql -d bta-devel -c 'delete from broadcasttranscodingrecord;'"
-#ssh $develHost 'bta/bin/enqueueJobs.sh Broadcast 0'
-#ssh $develHost 'bta/bin/queryChangesDoms.sh Broadcast > ~/queue$(date +"%y%m%d").txt'
-ssh $develHost 'head -n4 ~/queue$(date +"%y%m%d").txt > ~/queue.txt'
+ssh $develHost "psql -d bta-devel -c 'delete from broadcasttranscodingrecord;'"
+ssh $develHost "bta/bin/enqueueJobs.sh Broadcast $(date -d 'last month' +%s000)"
+ssh $develHost 'bta/bin/queryChangesDoms.sh Broadcast > ~/queue$(date +"%y%m%d").txt'
+ssh $develHost 'head -n10 ~/queue$(date +"%y%m%d").txt > ~/queue.txt'
+#ssh $develHost 'head -n100 ~/queue180508.txt > ~/queue.txt'
 ssh $develHost 'bta/bin/transcode-master.sh start -h localhost -n 1 -j ~/queue.txt -v'
 
 
 # Stop trancodering:
 # bta/bin/transcode-master.sh stop
 # ps -ef | grep transcoder | grep -v grep| sed 's/ \+/ /g' | cut -d' ' -f2 | xargs -r kill
+
+#Info to port prod programs to devel
+#http://naiad:7880/fedora/objects?pid=true&label=true&title=true&identifier=true&terms=&query=label~*teracom*&maxResults=20#
+#
+#curl -H "Content-Type: text/xml" -ufedoraAdmin:fedoraAdminPass http://alhena:7980/fedora/objects/new -X POST --data-binary @objectXML1.xml -vvv
