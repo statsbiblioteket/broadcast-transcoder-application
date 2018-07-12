362bef7e1337cc9696f99e1851bc237441685a77
Asger Askov Blekinge <abr@kb.dk>
2018-07-12 18:22:15 +0200

    javadoc for file formats

--- broadcast-transcoder/src/main/java/dk/statsbiblioteket/broadcasttranscoder/util/FileFormatEnum.java
+++ broadcast-transcoder/src/main/java/dk/statsbiblioteket/broadcasttranscoder/util/FileFormatEnum.java
@@ -6,7 +6,28 @@ package dk.statsbiblioteket.broadcasttranscoder.util;
  * Date: 9/26/12
  * Time: 10:18 AM
  * To change this template use File | Settings | File Templates.
+ *
+ * @see dk.statsbiblioteket.broadcasttranscoder.processors.FilePropertiesIdentifierProcessor
  */
 public enum FileFormatEnum {
-    AUDIO_WAV, MPEG_PS, MULTI_PROGRAM_MUX, SINGLE_PROGRAM_VIDEO_TS, SINGLE_PROGRAM_AUDIO_TS;
+    /**
+     * *.wav files
+     */
+    AUDIO_WAV,
+    /**
+     * *.mpg and *.mpeg files
+     */
+    MPEG_PS,
+    /**
+     * starts with mux and ends with .ts
+     */
+    MULTI_PROGRAM_MUX,
+    /**
+     * ends with .ts and bitrate > 100k
+     */
+    SINGLE_PROGRAM_VIDEO_TS,
+    /**
+     * ends with .ts and bitrate < 100k
+     */
+    SINGLE_PROGRAM_AUDIO_TS;
 }
