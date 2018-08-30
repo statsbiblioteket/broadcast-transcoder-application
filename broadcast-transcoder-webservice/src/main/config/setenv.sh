89d045b49031cd6bb61b89a4e97cd15ff0318589
Asger Askov Blekinge <abr@kb.dk>
2018-08-30 10:58:43 +0200

    setenv use the right SCL way

--- broadcast-transcoder-webservice/src/main/config/setenv.sh
+++ broadcast-transcoder-webservice/src/main/config/setenv.sh
@@ -1,6 +1,6 @@
 
-#Source this to get vlc and an OLD ffmpeg
-source /opt/rh/sb-btatools1/enable
+# Enable the sb-btatools1 SCL to get vlc 1.1.x (also includes ffmpeg 0.10)
+source scl_source enable sb-btatools1
 
 #Source this to get a newer ffmpeg
 #source /opt/ffmpeg34/enable
