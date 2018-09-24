ec9dbe2cb147a1316fd8897fa476f1fbbb561b05
Asger Askov Blekinge <abr@kb.dk>
2018-09-24 15:04:59 +0200

    enqueue job now uses upsert

--- enqueueJob.sh
+++ enqueueJob.sh
@@ -6,5 +6,5 @@ object="${1:-uuid:9b773c50-7e0e-4c42-ad18-e11ad9b47db2}"
 
 
 #Auth through ~/.pgpass
-psql --username=bta --dbname='bta-devel' --host=iapetus --echo-queries --command="INSERT INTO public.broadcasttranscodingrecord (id, domslatesttimestamp, failuremessage, lasttranscodedtimestamp, transcodingstate, broadcastendtime, broadcaststarttime, channel, endoffset, startoffset, title, transcodingcommand, tvmeter, video) VALUES ('$object', 1000, null, null, 0, null, null, null, 0, 0, null, null, true, true);"
+psql --username=bta --dbname='bta-devel' --host=iapetus --echo-queries --command="INSERT INTO public.broadcasttranscodingrecord (id, domslatesttimestamp, failuremessage, lasttranscodedtimestamp, transcodingstate, broadcastendtime, broadcaststarttime, channel, endoffset, startoffset, title, transcodingcommand, tvmeter, video) VALUES ('$object', 1000, null, null, 0, null, null, null, 0, 0, null, null, true, true) ON CONFLICT (id) DO UPDATE SET domslatesttimestamp=1000, transcodingstate=0;"
 
